14f3b49eb1094c4b01b807ea5d4cc64ebc2dc12a
Charles Wang <s3894453@student.rmit.edu.au>
2025-09-11 03:25:23 +1000

    check for commit

--- app/src/main/java/com/example/application/Login.kt
+++ app/src/main/java/com/example/application/Login.kt
@@ -12,6 +12,6 @@ import androidx.compose.ui.Modifier
 @Composable
 fun LoginScreen()    {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Check", style = MaterialTheme.typography.headlineMedium)
+        Text("Che1ck", style = MaterialTheme.typography.headlineMedium)
     }
 }
